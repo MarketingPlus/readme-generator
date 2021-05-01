ad30ca064cdcb8b560a12fd7c97816d7cecdbbb3
MarketingPlus <benmarketingplus@gmail.com>
2021-05-01 20:28:34 +1000

    added small amount of code to see if my question would appear when typing node index, just to check weither it would appear

--- index.js
+++ index.js
@@ -21,13 +21,17 @@ const questions = [
 
 
 
+
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
